a77bab7a4b4dc3c4383dd7a99f1889754ea95897
Jochen Berger <fooberger@gmail.com>
2016-08-15 15:52:49 +0200

    hack the classloader to make Clojure happy

--- src/main/groovy/com/github/jochenberger/gradlelein/LeinPlugin.groovy
+++ src/main/groovy/com/github/jochenberger/gradlelein/LeinPlugin.groovy
@@ -21,66 +21,72 @@ class LeinPlugin implements Plugin<Project>{
   public void apply(Project p) {
     p.logger.debug("Applying plugin")
     p.extensions.create('lein', LeinPluginExtension)
-    Var require = RT.var("clojure.core", "require");
-    require.invoke(Symbol.create("leiningen.core.main"));
-    require.invoke(Symbol.create("leiningen.core.user"));
-    require.invoke(Symbol.create("leiningen.core.project"));
-    require.invoke(Symbol.create("leiningen.core.classpath"));
-    Var userInit = Var.find(Symbol.create("leiningen.core.user/init"));
-    p.logger.debug("Initializing Leiningen")
+    def previousClassLoader = Thread.currentThread().contextClassLoader
+    try {
+      Thread.currentThread().contextClassLoader = RT.class.classLoader
+      Var require = RT.var("clojure.core", "require");
+      require.invoke(Symbol.create("leiningen.core.main"));
+      require.invoke(Symbol.create("leiningen.core.user"));
+      require.invoke(Symbol.create("leiningen.core.project"));
+      require.invoke(Symbol.create("leiningen.core.classpath"));
+      Var userInit = Var.find(Symbol.create("leiningen.core.user/init"));
+      p.logger.debug("Initializing Leiningen")
 
-    userInit.invoke();
-    p.logger.debug("Reading project")
-    Var readProject = Var.find(Symbol.create("leiningen.core.project/read"));
+      userInit.invoke();
+      p.logger.debug("Reading project")
+      Var readProject = Var.find(Symbol.create("leiningen.core.project/read"));
 
-    Map cljProject = (Map) readProject.invoke(p.file('project.clj').absolutePath);
+      Map cljProject = (Map) readProject.invoke(p.file('project.clj').absolutePath);
 
-    p.logger.info("Project = {}", cljProject);
+      p.logger.info("Project = {}", cljProject);
 
-    List deps = cljProject.get(Keyword.find("dependencies"));
-    def addedConfigurations = [];
+      List deps = cljProject.get(Keyword.find("dependencies"));
+      def addedConfigurations = [];
 
-    deps.each { Object o->
-      PersistentVector dep = (PersistentVector) o;
-      Symbol name = (Symbol) dep.get(0);
+      deps.each { Object o->
+        PersistentVector dep = (PersistentVector) o;
+        Symbol name = (Symbol) dep.get(0);
 
-      String groupId = name.getNamespace();
-      String artifactId = name.getName();
-      String version = dep.get(1);
-      p.logger.info("dep = {} ", dep);
+        String groupId = name.getNamespace();
+        String artifactId = name.getName();
+        String version = dep.get(1);
+        p.logger.info("dep = {} ", dep);
 
-      int indexOfScope = dep.indexOf(Keyword.find("scope"));
-      String scope = (String) (indexOfScope >= 0 ? dep.get(indexOfScope + 1) : null);
+        int indexOfScope = dep.indexOf(Keyword.find("scope"));
+        String scope = (String) (indexOfScope >= 0 ? dep.get(indexOfScope + 1) : null);
 
-      if (scope == null) {
-        scope = "compile";
-      }
-      p.logger.debug("scope = {}", scope);
+        if (scope == null) {
+          scope = "compile";
+        }
+        p.logger.debug("scope = {}", scope);
 
-      List exclusions = null
-      int indexOfExclusions = dep.indexOf(Keyword.find("exclusions"));
-      if (indexOfExclusions >= 0) {
-        exclusions = (List) dep.get(indexOfExclusions + 1);
-        p.logger.debug("Exclusions = {}" , exclusions);
-      }
+        List exclusions = null
+        int indexOfExclusions = dep.indexOf(Keyword.find("exclusions"));
+        if (indexOfExclusions >= 0) {
+          exclusions = (List) dep.get(indexOfExclusions + 1);
+          p.logger.debug("Exclusions = {}" , exclusions);
+        }
 
-      String gradleDepString = "$groupId:$artifactId:$version";
-      if (!addedConfigurations.contains(scope)){
-        p.logger.debug("Adding configuration {}" , scope);
-        addedConfigurations.add(scope)
-        p.configurations.create(scope)
-      }
-      ModuleDependency dependency = (ModuleDependency) p.dependencies.add(scope, gradleDepString)
-      if (exclusions !=null){
-        exclusions.each { Object eo->
-          PersistentVector exDep = (PersistentVector) eo;
-          Symbol exName = (Symbol) exDep.get(0);
-          String exGroupId = exName.getNamespace();
-          String exArtifactId = exName.getName();
-          p.logger.debug("EXCLUDE $exGroupId:$exArtifactId")
-          dependency.exclude group: exGroupId, module: exArtifactId
+        String gradleDepString = "$groupId:$artifactId:$version";
+        if (!addedConfigurations.contains(scope)){
+          p.logger.debug("Adding configuration {}" , scope);
+          addedConfigurations.add(scope)
+          p.configurations.create(scope)
+        }
+        ModuleDependency dependency = (ModuleDependency) p.dependencies.add(scope, gradleDepString)
+        if (exclusions !=null){
+          exclusions.each { Object eo->
+            PersistentVector exDep = (PersistentVector) eo;
+            Symbol exName = (Symbol) exDep.get(0);
+            String exGroupId = exName.getNamespace();
+            String exArtifactId = exName.getName();
+            p.logger.debug("EXCLUDE $exGroupId:$exArtifactId")
+            dependency.exclude group: exGroupId, module: exArtifactId
+          }
         }
       }
+    }finally{
+      Thread.currentThread().contextClassLoader = previousClassLoader
     }
   }
 }
